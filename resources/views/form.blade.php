c81da227f25b5886bfaad6f712a6b9dfe4ec3993
Shion <2190231@ecccomp.com>
2020-11-30 16:20:37 +0900

    uhu

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -16,38 +16,36 @@
     </head>
 
     <body>
-        <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px;">
+        <div style="background-color:#e0ffff; padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px;">
             性別を選択してください
         </div>
         <div>
             <input type="radio" name="gender" value="male">男性
             <input type="radio" name="gender" value="female">女性<br>
         </div>
-        <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px;">
+        <div style="background-color:#e0ffff; padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px;">
             年齢を入力してください
         </div>
         <div>
             (満：
-            <input type="text" size="3" name="old"> 歳)
+            <input type="number" size="3" name="old"> 歳)
         </div>
-        <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px;">
+        <div style="background-color:#e0ffff; padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px;">
             現在の収入を入力してください
         </div>
         <div>
-            <input type="text" size="5" name="income"> 円
+            <input type="number" size="5" name="income"> 円
         </div>
-        <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px;">
+        <div style="background-color:#e0ffff; padding: 10px; margin-bottom: 10px; border: 1px solid #333333; border-radius: 10px;">
             結婚はしていますか？
         </div>
-
 
           </head>
 
           <body>
           <form name="Sample3_form" action="idomu_sample.html">
-          <input id="tatakau" name="tatakau" type="radio" value = "している"/>している<br>
-          <input id="nigeru" name="tatakau"  type="radio" value = "していない"/>していない<br>
-          <input type="button" value="診断する" onclick="marry()">
+          <input id="tatakau" name="tatakau" type="radio" value = "している" onclick="marry()"/>している<br>
+          <input id="nigeru" name="tatakau"  type="radio" value = "していない" onclick="marry()"/>していない<br>
           </form>
           </body>
         <script src="public/js/reel.js"></script>
