9652ddf576a01bac6a11184557ec397a7db9dede
Ken Hatakenaka <2190158@ecccomp.com>
2020-11-30 15:43:24 +0900

    test

--- public/js/reel.js
+++ public/js/reel.js
@@ -1,5 +1,5 @@
 function marry(){
-    if ( document.Sample3_form.tatakau.valzzyyyue == "している") {
+    if ( document.Sample3_form.tatakau.value == "している") {
        document.write("してるの！");
     }else{
     document.write("してないんかい！");
